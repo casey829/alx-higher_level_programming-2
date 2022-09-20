740e4a4022d3589f09f446becd1ffa01237a15fc
casey829 <caseymwangi31@gmail.com>
2022-09-20 12:19:26 -0700

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/bin/baist_digit(number):
+    if number >= 0:
+        l_digit = number % 10
+    else:
+        l_digit = number % -10
+        l_digit *= -1
+    print("{:d}".format(l_digit), end='')
+    return (l_digit)
