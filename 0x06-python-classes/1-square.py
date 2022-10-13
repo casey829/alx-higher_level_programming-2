99b1c82a507f146f5ce7e69c92c13ff19d80816d
casey829 <caseymwangi31@gmail.com>
2022-10-13 13:11:53 -0700

    add file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 class Square:
-    """Class Square that has attributes. Instantiation with size
-    Attributes:
-        size (int): The size of the square
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
     """
+
     def __init__(self, size):
-        """The __init__ method for Square class
-        Args:
-            size: (:obj: 'int'): A private instance size
-        """
+        """Initializes the data."""
         self.__size = size
